ab03f5d30386cc7ba0fc23b605e9b7f25229ad6c
Iuri Saad <143546576+saadiuri@users.noreply.github.com>
2025-11-08 16:10:42 -0300

    Adicionando testes

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+export default {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+    testMatch: ["**/tests/**/*.test.ts"],
+};
